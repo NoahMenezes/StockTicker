8ec64c62ae0895e21d5f1793e4b77cd2e96fe0a7
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-29 23:48:14 +0530

    ui fixed back

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="echo-navbar">
+        <nav className='echo-navbar'>
             <div className="navbar-container">
-                <a href="/" className="navbar-heading">
-                    echoTicker
-                </a>
+                <a href="/" className="navbar-heading">EchoTicker</a>
+                <div className="status-indicator">
+                    <span className="status-dot"></span>
+                    <span>Online</span>
+                </div>
             </div>
         </nav>
     );
